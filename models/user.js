c91b56f176f51add4562930ef3e4de707fb1d5f7
Ayotomiwa Adekunle <ayotomiwaadekunle7808@gmail.com>
2023-02-08 12:33:49 +0000

    Create user model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema, model } from "mongoose";
+import passportLocalMongoose from 'passport-local-mongoose';
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+export default model('User', userSchema);

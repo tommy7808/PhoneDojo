8107f530f88b83fa3587c5f5c16c2b408c38bd85
Ayotomiwa Adekunle <ayotomiwaadekunle7808@gmail.com>
2023-02-12 18:05:49 +0000

    Refactor reviews routes into controllers

---
+++ controllers/reviews.js
@@ -0,0 +1,34 @@
+import Phone from '../models/phone.js';
+import Review from '../models/review.js';
+import AppError from '../utils/AppError.js';
+
+export const createReview = async (req, res, next) => {
+    try {
+        const { phoneId } = req.params;
+        const phone = await Phone.findById(phoneId);
+        if (!phone) {
+            throw new AppError(404, 'Phone not found');
+        }
+        const review = new Review(req.body);
+        review.user = req.user._id;
+        await review.save();
+        phone.reviews.push(review);
+        await phone.save();
+        req.flash('success', 'Successfully posted a review');
+        res.redirect(`/phones/${phoneId}`);
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const deleteReview = async (req, res, next) => {
+    try {
+        const { phoneId, reviewID } = req.params;
+        await Phone.findByIdAndUpdate(phoneId, { $pull: { reviews: reviewID } });
+        await Review.findByIdAndDelete(reviewID);
+        req.flash('success', 'Successfully deleted a review');
+        res.redirect(`/phones/${phoneId}`);
+    } catch (err) {
+        next(err);
+    }
+}
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,40 +1,10 @@
 import express from 'express';
-import Phone from '../models/phone.js';
-import Review from '../models/review.js';
-import AppError from '../utils/AppError.js';
 import { isLoggedIn, isAuthorisedReview } from '../utils/middleware.js';
+import { createReview, deleteReview } from '../controllers/reviews.js';
 
 const router = express.Router({ mergeParams: true });
 
-router.post('/', isLoggedIn, async (req, res, next) => {
-    try {
-        const { phoneId } = req.params;
-        const phone = await Phone.findById(phoneId);
-        if (!phone) {
-            throw new AppError(404, 'Phone not found');
-        }
-        const review = new Review(req.body);
-        review.user = req.user._id;
-        await review.save();
-        phone.reviews.push(review);
-        await phone.save();
-        req.flash('success', 'Successfully posted a review');
-        res.redirect(`/phones/${phoneId}`);
-    } catch (err) {
-        next(err);
-    }
-});
-
-router.delete('/:reviewID', isLoggedIn, isAuthorisedReview, async (req, res, next) => {
-    try {
-        const { phoneId, reviewID } = req.params;
-        await Phone.findByIdAndUpdate(phoneId, { $pull: { reviews: reviewID } });
-        await Review.findByIdAndDelete(reviewID);
-        req.flash('success', 'Successfully deleted a review');
-        res.redirect(`/phones/${phoneId}`);
-    } catch (err) {
-        next(err);
-    }
-});
+router.post('/', isLoggedIn, createReview);
+router.delete('/:reviewID', isLoggedIn, isAuthorisedReview, deleteReview);
 
 export default router;

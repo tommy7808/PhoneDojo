de51d27fc48bb03031303ae00ee5ee702918e451
Ayotomiwa Adekunle <ayotomiwaadekunle7808@gmail.com>
2023-03-01 23:46:16 +0000

    Added more env vars

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-    // Load environment variables
-    require('dotenv').config();
-}
-
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
